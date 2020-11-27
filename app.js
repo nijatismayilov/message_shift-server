e1521222014a24923a2e20426ad6de416de69bf4
Nijat ISmayilov <nicatismayilov43@gmail.com>
2020-11-27 13:14:45 +0400

    HotFix: changed port

--- app.js
+++ app.js
@@ -31,6 +31,6 @@ app.use((err, req, res, next) => {
 	});
 });
 
-const PORT = process.env.PORT || 5000;
+const PORT = process.env.PORT || 8080;
 
 app.listen(PORT, console.log(`Server running on the port ${PORT}`));
